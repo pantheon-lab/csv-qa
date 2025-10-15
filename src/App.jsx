1bbadc88c083c65a462cc23803eb037b48d89f77
Anson Chan <anson@pantheonlab.ai>
2025-10-15 11:17:46 +0800

    fix: remove unnecessary text

--- src/App.jsx
+++ src/App.jsx
@@ -1144,13 +1144,6 @@ What is your art?,Performance about memory and relations,Something else,, ,2,Off
                 {/* Status + Comment */}
                 {statusCol && (
                   <Stack direction="row" spacing={1} alignItems="center">
-                    <Typography variant="body2" color="text.secondary">
-                      Status{" "}
-                      {useCheckedStatus && checkedColName
-                        ? `(editing ${checkedColName})`
-                        : ""}
-                      :
-                    </Typography>
                     {useCheckedStatus && checkedColName ? (
                       <Stack
                         direction="row"
